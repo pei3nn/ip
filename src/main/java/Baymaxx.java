565696a732877fb7f0090cdef2ff6f5133c98a9b
Pei En <peien2005@gmail.com>
2025-08-21 19:32:45 +0800

    Renamed chatbot, Added Greeting and Exit.

---
+++ src/main/java/Baymaxx.java
@@ -0,0 +1,13 @@
+public class Baymaxx {
+    public static void main(String[] args) {
+        String Greeting = "Hi! I'm Baymaxx (O-O) \n"
+                + "Your personal chatbot assistant \n"
+                + "What can I do for you? \n";
+        String Exit = "Goodbye. Hope to see you again soon! (O-O) \n";
+        System.out.println("___________________________________________\n"
+        + Greeting
+        + "___________________________________________\n"
+        + Exit
+        + "___________________________________________");
+    }
+}
