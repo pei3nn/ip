d59e96aa71b8e1630050f7b756d7ec7cfe018c4d
Pei En <peien2005@gmail.com>
2025-08-22 15:19:58 +0800

    Added TaskCollection class.

---
+++ src/main/java/TaskCollection.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskCollection {
+    private List<Task> tasks;
+
+    public TaskCollection() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskCollection(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task t) {
+        this.tasks.add(t);
+    }
+
+    public void removeTask(int index) {
+        this.tasks.remove(index);
+    }
+
+    public Task getTask(int index) {
+        return this.tasks.get(index);
+    }
+
+    public List<Task> getAllTasks() {
+        return this.tasks;
+    }
+
+    public int getSize() {
+        return this.tasks.size();
+    }
+}
